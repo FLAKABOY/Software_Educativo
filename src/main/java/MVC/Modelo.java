1373ba596c91c7237ed8d9379654f0c61c772f05
FLAKABOY <flakaboyestrada@gmail.com>
2023-11-25 22:58:41 -0700

    Se comienza con la programacion del modelo Se comienza a programar los metodos que realizaran acciones en la DB

--- src/main/java/MVC/Modelo.java
+++ src/main/java/MVC/Modelo.java
@@ -1,8 +1,15 @@
 /*
- * Esta clase se encarga de controlar todo de la BD
- * 
+ * Rendon Estrada Jorge
+ * Fecha: 25/11/2023
+ * Descripcion: Clase que tendra los metodos de conexion y acciones que se
+ * realizaran en la Base de Datos
  */
 package MVC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -10,4 +17,25 @@ package MVC;
  */
 public class Modelo {
     
+    //Metodo que realiza la conexion a la DB
+    public static Connection conectar(){
+        Connection connection = null;
+        try{
+            //Primer paso programar el controlador JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            //Establecer la conexión
+            String url = "jdbc:mysql://url:puerto/nombreDB";
+            String user = "";
+            String password = "";
+            connection = DriverManager.getConnection(url, user, password);
+            
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException | ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error en la conexión");
+            e.printStackTrace();
+            // Manejo de excepciones
+        }
+        return connection;
+    }
 }
